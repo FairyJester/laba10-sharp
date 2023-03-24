1ad46d320951d28afd0aefa57ee383a0034932ff
FairyJester <98344500+FairyJester@users.noreply.github.com>
2023-03-24 10:38:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace laba10_sharp
+{
+
+
+    public class Car
+    {
+        public string CarName { set; get; }
+    }
+
+    public class Garege
+    {
+        public List<Car> garage = new List<Car>();
+    }
+
+    public class Washer
+    {
+        public delegate void WashCars(Car car);
+
+        public WashCars washCars = new WashCars(wash);  
+
+        public static void wash(Car car)
+        {
+            Console.WriteLine($" в автомойке ваш {car.CarName}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Opel = new Car();
+            Car Volvo = new Car();
+            Car Nissan = new Car();
+
+            Opel.CarName = "Opel Antara";
+            Volvo.CarName = "Volvo s90";
+            Nissan.CarName = "Nissan Juke";
+
+            Garege garege = new Garege();
+            garege.garage.Add(Opel);
+            garege.garage.Add(Volvo);
+            garege.garage.Add(Nissan);
+
+            Washer washer = new Washer();
+
+            foreach(var car in garege.garage)
+            {
+                washer.washCars(car);
+            }
+            Console.ReadLine();
+        }
+    }
+}
